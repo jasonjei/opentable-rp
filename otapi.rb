9a0f6e3f77d9ad6862c57a51ed178c72208b9bba
Jason Hung <jasonjei@gmail.com>
2015-01-09 19:02:42 -0800

    Progress addition

--- otapi.rb
+++ otapi.rb
@@ -15,6 +15,22 @@ get '/otapi_v3.ashx/*' do
 	m.get(url).body	
 end
 
+get '/otcredentials' do
+  m = Mechanize.new { |mech| mech.user_agent = 'iPhone' }
+  token_page = m.get "https://mobile-api.opentable.com/oauth/consumer/token?client_id=ot-anonymous-apps&client_secret=0pentab1e&grant_type=implicit"
+  token = token_page.body
+end
+
+
+get '/availability' do
+  m = Mechanize.new
+  m.request_headers["Authorization"] = "Bearer #{params[:token]}"
+
+  res = m.put("https://mobile-api.opentable.com/api/v2/restaurant/availability", 
+        "{\"allowPop\":false,\"partySize\":#{params[:party]},\"includeOffers\":true,\"requestDateMessages\":true,\"rids\":[\"#{params[:rid]}\"],\"dateTime\":\"#{params[:time]}\",\"attribution\":{\"partnerId\":\"84\"}}", {'Content-Type' => 'application/json; charset=utf-8'})
+  res.body
+end
+
 get '/creditcard' do
   m = Mechanize.new { |mech| mech.user_agent = 'iPhone' }
   url = params[:url]
@@ -57,6 +73,42 @@ get '/ip' do
 	b.match(/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/).to_s
 end
 
+get '/progress' do
+    desired_at = Time.parse(params[:desired_at])
+  m = Mechanize.new { |mech| mech.user_agent = 'iPhone' }
+
+    search_page = m.get("http://www.opentable.com/restaurant-search.aspx?startDate=#{CGI.escape(desired_at.strftime("%m/%d/%Y").strip)}&ResTime=#{CGI.escape(desired_at.strftime("%l:%M %p").strip)}&PartySize=#{params[:people]}&PartySizeFake=#{params[:people]}+People&RestaurantID=158713&rid=158713&GeoID=58&txtDateFormat=MM%2Fdd%2Fyyyy&RestaurantReferralID=158713", [], "http://theprogress-sf.com/reservations/")
+    
+
+    return nil if not search_page.forms.first
+    return nil if not search_page.forms.first.ClientTimeStamp
+
+    client_timestamp = search_page.forms.first.ClientTimeStamp
+    search_results = m.get("http://m.opentable.com/search/results?PartySize=#{params[:people]}&DateInvariantCulture=#{desired_at.to_date.iso8601}T00%3A00%3A00&TimeInvariantCulture=0001-01-01T#{CGI.escape(desired_at.strftime("%T").strip)}&SearchName=State+Bird+Provisions&MetroAreaID=&RegionID=&Latitude=&Longitude=&RestaurantID=158713&PromoID=&PromoType=&PartnerReferralID=&ConfirmationNumber=&ClientTimeStamp=#{client_timestamp}&OfferConfirmNumber=0&ChosenOfferId=0&ReservationStatus=")
+
+    avail_times = search_results.links.select { |v| v.uri.to_s =~ /slotlock/ }
+    return nil if avail_times.empty?
+    good_times = cleanup_web_times(avail_times,desired_at,params[:tolerance].to_f,params[:tolerance_sign].to_i)
+
+    if good_times.empty?
+      return nil
+    end
+
+    details_page = good_times.first.click
+    if not details_page.forms.first
+      return nil
+    end
+
+    details_page_form = details_page.forms.first
+    details_page_form.FirstName = "#{params[:first_name]}"
+    details_page_form.LastName = "#{params[:last_name]}"
+    details_page_form.Email = "#{params[:email]}"
+    details_page_form.PhoneNumber = "#{params[:phone]}"
+
+    confirm_page = details_page_form.submit details_page_form.buttons.detect { |v| v.value =~ /Confirm/ }
+    return confirm_page.body
+
+end
 
 get '/statebird' do
     desired_at = Time.parse(params[:desired_at])
