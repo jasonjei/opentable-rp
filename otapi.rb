1045d6b54377480a4d65886c05ae526ee74caf49
Jason Hung <jasonjei@gmail.com>
2015-08-08 19:02:32 -0700

    State Bird/Progress changes

--- otapi.rb
+++ otapi.rb
@@ -25,6 +25,7 @@ end
 get '/availability' do
   m = Mechanize.new
   m.request_headers["Authorization"] = "Bearer #{params[:token]}"
+  m.user_agent = "com.contextoptional.OpenTable/8.6.1.1; iPhone; iOS/8.1; 3; #{SecureRandom.uuid}"
 
   res = m.put("https://mobile-api.opentable.com/api/v2/restaurant/availability", 
         "{\"allowPop\":false,\"partySize\":#{params[:party]},\"includeOffers\":true,\"requestDateMessages\":true,\"rids\":[\"#{params[:rid]}\"],\"dateTime\":\"#{params[:time]}\",\"attribution\":{\"partnerId\":\"84\"}}", {'Content-Type' => 'application/json; charset=utf-8'})
@@ -75,10 +76,9 @@ end
 
 get '/progress' do
     desired_at = Time.parse(params[:desired_at])
-  m = Mechanize.new { |mech| mech.user_agent = 'iPhone' }
+    m = Mechanize.new { |mech| mech.user_agent = 'iPhone' }
 
-    search_page = m.get("http://www.opentable.com/restaurant-search.aspx?startDate=#{CGI.escape(desired_at.strftime("%m/%d/%Y").strip)}&ResTime=#{CGI.escape(desired_at.strftime("%l:%M %p").strip)}&PartySize=#{params[:people]}&PartySizeFake=#{params[:people]}+People&RestaurantID=158713&rid=158713&GeoID=58&txtDateFormat=MM%2Fdd%2Fyyyy&RestaurantReferralID=158713", [], "http://theprogress-sf.com/reservations/")
-    
+    search_page = m.get("http://www.opentable.com/restaurant-search.aspx?startDate=#{CGI.escape(desired_at.strftime("%m/%d/%Y").strip)}&ResTime=#{CGI.escape(desired_at.strftime("%l:%M %p").strip)}&PartySize=#{params[:people]}&PartySizeFake=#{params[:people]}+People&RestaurantID=158713&rid=158713&GeoID=58&txtDateFormat=MM%2Fdd%2Fyyyy&RestaurantReferralID=158713", [], "http://theprogress-sf.com/")
 
     return nil if not search_page.forms.first
     return nil if not search_page.forms.first.ClientTimeStamp
@@ -87,6 +87,7 @@ get '/progress' do
     search_results = m.get("http://m.opentable.com/search/results?PartySize=#{params[:people]}&DateInvariantCulture=#{desired_at.to_date.iso8601}T00%3A00%3A00&TimeInvariantCulture=0001-01-01T#{CGI.escape(desired_at.strftime("%T").strip)}&SearchName=State+Bird+Provisions&MetroAreaID=&RegionID=&Latitude=&Longitude=&RestaurantID=158713&PromoID=&PromoType=&PartnerReferralID=&ConfirmationNumber=&ClientTimeStamp=#{client_timestamp}&OfferConfirmNumber=0&ChosenOfferId=0&ReservationStatus=")
 
     avail_times = search_results.links.select { |v| v.uri.to_s =~ /slotlock/ }
+
     return nil if avail_times.empty?
     good_times = cleanup_web_times(avail_times,desired_at,params[:tolerance].to_f,params[:tolerance_sign].to_i)
 
@@ -100,6 +101,47 @@ get '/progress' do
     end
 
     details_page_form = details_page.forms.first
+
+    if details_page_form.field_with(:name => "CreditCardNumber")
+      # do CC stuff
+      #  => ["AMEX", "Diners Club", "Discover", "MasterCard", "Visa"] 
+      ccn = params[:ccn]
+      ccm = params[:ccm]
+      ccy = params[:ccy]
+
+      return "needcc" if ccn.to_s.strip.empty?
+
+      ccform = details_page.forms.last
+      ccform.add_field!("hash", details_page_form.field_with(name: "hash").value)
+      ccform.add_field!("key_id", details_page_form.key_id)
+      ccform.add_field!("time", details_page_form.time)
+      ccform.add_field!("redirect", details_page_form.redirect)
+      ccform.add_field!("customer[payment_method][type]", "CreditCard")
+      ccform.add_field!("customer[payment_method][number]", ccn)
+      ccform.add_field!("customer[payment_method][expiration_month]", ccm)
+      ccform.add_field!("customer[payment_method][expiration_year]", ccy)
+      ccform.add_field!("customer[first_name]", params[:first_name])
+      ccform.add_field!("customer[last_name]", params[:last_name])
+      
+      result = ccform.submit
+
+      dpq = CGI::parse(details_page.uri.query) # details_page.uri.query
+      vq = CGI::parse(result.uri.to_s)
+
+      constructlink = "https://m.opentable.com/reservation/details?RestaurantID=#{dpq["RestaurantID"].join}&" + 
+      "Points=#{dpq["Points"].join}&PointsType=#{dpq["PointsType"].join}&SlotHash=#{dpq["SlotHash"].join}&SecurityID=#{dpq["SecurityID"].join}&" + 
+      "DateTime=#{URI.encode(dpq["DateTime"].join)}&PartySize=#{dpq["PartySize"].join}&SlotLockID=#{dpq["SlotLockID"].join}&OfferConfirmNumber=#{dpq["OfferConfirmNumber"].join}&" + 
+      "ChosenOfferId=#{dpq["ChosenOfferId"].join}&IsCreditCard=#{dpq["IsCreditCard"].join}&IsMiddleSlot=#{dpq["IsMiddleSlot"].join}&ArePopPoints=#{dpq["ArePopPoints"].join}&ExpireMonth=#{ccm}&" + 
+      "ExpireYear=#{ccy}&Last4=#{ccn[-4..-1]}&PaymentUuid=#{vq["customer[payment_method][uuid]"].join}&" + 
+      "CustomerUuid=#{vq["customer[uuid]"].join}&" + 
+      "ResponseHash=#{vq["hash"].join}&HttpStatus=#{vq["http_status"].join}&" + 
+      "ResponseTime=#{vq["time"].join}"
+
+      details_page = m.get constructlink
+    end
+
+    details_page_form = details_page.forms.first
+
     details_page_form.FirstName = "#{params[:first_name]}"
     details_page_form.LastName = "#{params[:last_name]}"
     details_page_form.Email = "#{params[:email]}"
@@ -107,16 +149,46 @@ get '/progress' do
 
     confirm_page = details_page_form.submit details_page_form.buttons.detect { |v| v.value =~ /Confirm/ }
     return confirm_page.body
+end
 
+get '/progresstimes' do
+  desired_at = Time.parse(params[:desired_at])
+  m = Mechanize.new { |mech| mech.user_agent = 'iPhone' }
+
+  search_page = m.get("http://www.opentable.com/restaurant-search.aspx?startDate=#{CGI.escape(desired_at.strftime("%m/%d/%Y").strip)}&ResTime=#{CGI.escape(desired_at.strftime("%l:%M %p").strip)}&PartySize=#{params[:people]}&PartySizeFake=#{params[:people]}+People&RestaurantID=158713&rid=158713&GeoID=58&txtDateFormat=MM%2Fdd%2Fyyyy&RestaurantReferralID=158713", [], "http://theprogress-sf.com/")
+  
+  return nil if not search_page.forms.first
+  return nil if not search_page.forms.first.ClientTimeStamp
+
+  client_timestamp = search_page.forms.first.ClientTimeStamp
+  search_results = m.get("http://m.opentable.com/search/results?PartySize=#{params[:people]}&DateInvariantCulture=#{desired_at.to_date.iso8601}T00%3A00%3A00&TimeInvariantCulture=0001-01-01T#{CGI.escape(desired_at.strftime("%T").strip)}&SearchName=State+Bird+Provisions&MetroAreaID=&RegionID=&Latitude=&Longitude=&RestaurantID=158713&PromoID=&PromoType=&PartnerReferralID=&ConfirmationNumber=&ClientTimeStamp=#{client_timestamp}&OfferConfirmNumber=0&ChosenOfferId=0&ReservationStatus=")
+
+  avail_times = search_results.links.select { |v| v.uri.to_s =~ /slotlock/ }
+  return JSON.dump(avail_times.map { |v| Chronic.parse(CGI.parse(URI.parse(v.uri.to_s).query)["DateTime"].join('')) })
+end
+
+get '/statebirdtimes' do
+  desired_at = Time.parse(params[:desired_at])
+  m = Mechanize.new { |mech| mech.user_agent = 'iPhone' }
+
+  search_page = m.get("http://www.opentable.com/restaurant-search.aspx?startDate=#{CGI.escape(desired_at.strftime("%m/%d/%Y").strip)}&ResTime=#{CGI.escape(desired_at.strftime("%l:%M %p").strip)}&PartySize=#{params[:people]}&PartySizeFake=#{params[:people]}+People&RestaurantID=139246&rid=139246&GeoID=58&txtDateFormat=MM%2Fdd%2Fyyyy&RestaurantReferralID=139246", [], "http://mobile.dudamobile.com/site/statebirdsfcom?url=http%3A%2F%2Fstatebirdsf.com%2Freservations%2F")
+  
+  return nil if not search_page.forms.first
+  return nil if not search_page.forms.first.ClientTimeStamp
+
+  client_timestamp = search_page.forms.first.ClientTimeStamp
+  search_results = m.get("http://m.opentable.com/search/results?PartySize=#{params[:people]}&DateInvariantCulture=#{desired_at.to_date.iso8601}T00%3A00%3A00&TimeInvariantCulture=0001-01-01T#{CGI.escape(desired_at.strftime("%T").strip)}&SearchName=State+Bird+Provisions&MetroAreaID=&RegionID=&Latitude=&Longitude=&RestaurantID=139246&PromoID=&PromoType=&PartnerReferralID=&ConfirmationNumber=&ClientTimeStamp=#{client_timestamp}&OfferConfirmNumber=0&ChosenOfferId=0&ReservationStatus=")
+
+  avail_times = search_results.links.select { |v| v.uri.to_s =~ /slotlock/ }
+  return JSON.dump(avail_times.map { |v| Chronic.parse(CGI.parse(URI.parse(v.uri.to_s).query)["DateTime"].join('')) })
 end
 
 get '/statebird' do
     desired_at = Time.parse(params[:desired_at])
-	m = Mechanize.new { |mech| mech.user_agent = 'iPhone' }
+    m = Mechanize.new { |mech| mech.user_agent = 'iPhone' }
 
     search_page = m.get("http://www.opentable.com/restaurant-search.aspx?startDate=#{CGI.escape(desired_at.strftime("%m/%d/%Y").strip)}&ResTime=#{CGI.escape(desired_at.strftime("%l:%M %p").strip)}&PartySize=#{params[:people]}&PartySizeFake=#{params[:people]}+People&RestaurantID=139246&rid=139246&GeoID=58&txtDateFormat=MM%2Fdd%2Fyyyy&RestaurantReferralID=139246", [], "http://mobile.dudamobile.com/site/statebirdsfcom?url=http%3A%2F%2Fstatebirdsf.com%2Freservations%2F")
     
-
     return nil if not search_page.forms.first
     return nil if not search_page.forms.first.ClientTimeStamp
 
@@ -137,6 +209,47 @@ get '/statebird' do
     end
 
     details_page_form = details_page.forms.first
+
+    if details_page_form.field_with(:name => "CreditCardNumber")
+      # do CC stuff
+      #  => ["AMEX", "Diners Club", "Discover", "MasterCard", "Visa"] 
+      ccn = params[:ccn]
+      ccm = params[:ccm]
+      ccy = params[:ccy]
+
+      return "needcc" if ccn.to_s.strip.empty?
+
+      ccform = details_page.forms.last
+      ccform.add_field!("hash", details_page_form.field_with(name: "hash").value)
+      ccform.add_field!("key_id", details_page_form.key_id)
+      ccform.add_field!("time", details_page_form.time)
+      ccform.add_field!("redirect", details_page_form.redirect)
+      ccform.add_field!("customer[payment_method][type]", "CreditCard")
+      ccform.add_field!("customer[payment_method][number]", ccn)
+      ccform.add_field!("customer[payment_method][expiration_month]", ccm)
+      ccform.add_field!("customer[payment_method][expiration_year]", ccy)
+      ccform.add_field!("customer[first_name]", params[:first_name])
+      ccform.add_field!("customer[last_name]", params[:last_name])
+      
+      result = ccform.submit
+
+      dpq = CGI::parse(details_page.uri.query) # details_page.uri.query
+      vq = CGI::parse(result.uri.to_s)
+
+      constructlink = "https://m.opentable.com/reservation/details?RestaurantID=#{dpq["RestaurantID"].join}&" + 
+      "Points=#{dpq["Points"].join}&PointsType=#{dpq["PointsType"].join}&SlotHash=#{dpq["SlotHash"].join}&SecurityID=#{dpq["SecurityID"].join}&" + 
+      "DateTime=#{URI.encode(dpq["DateTime"].join)}&PartySize=#{dpq["PartySize"].join}&SlotLockID=#{dpq["SlotLockID"].join}&OfferConfirmNumber=#{dpq["OfferConfirmNumber"].join}&" + 
+      "ChosenOfferId=#{dpq["ChosenOfferId"].join}&IsCreditCard=#{dpq["IsCreditCard"].join}&IsMiddleSlot=#{dpq["IsMiddleSlot"].join}&ArePopPoints=#{dpq["ArePopPoints"].join}&ExpireMonth=#{ccm}&" + 
+      "ExpireYear=#{ccy}&Last4=#{ccn[-4..-1]}&PaymentUuid=#{vq["customer[payment_method][uuid]"].join}&" + 
+      "CustomerUuid=#{vq["customer[uuid]"].join}&" + 
+      "ResponseHash=#{vq["hash"].join}&HttpStatus=#{vq["http_status"].join}&" + 
+      "ResponseTime=#{vq["time"].join}"
+
+      details_page = m.get constructlink
+    end
+
+    details_page_form = details_page.forms.first
+
     details_page_form.FirstName = "#{params[:first_name]}"
     details_page_form.LastName = "#{params[:last_name]}"
     details_page_form.Email = "#{params[:email]}"
@@ -144,7 +257,6 @@ get '/statebird' do
 
     confirm_page = details_page_form.submit details_page_form.buttons.detect { |v| v.value =~ /Confirm/ }
     return confirm_page.body
-
 end
 
 def cleanup_web_times(links,desired_at,tolerance,tolerance_sign)
